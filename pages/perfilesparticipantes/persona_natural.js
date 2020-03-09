5d6bc7fd4216c9d162d6de79c764b2dc06ba4c2e
cesar.augusto.britto <cesar.augusto.britto@gmail.com>
2020-03-09 10:12:44 -0500

    Ajustes modulos perfiles

--- pages/perfilesparticipantes/persona_natural.js
+++ pages/perfilesparticipantes/persona_natural.js
@@ -325,16 +325,12 @@ function validator_form(token_actual) {
                                         notify("danger", "ok", "Persona natural:", "Se registro un error, comuníquese con la mesa de ayuda soporte.convocatorias@scrd.gov.co");
                                     } else
                                     {
-<<<<<<< HEAD
                                       /**
                                       *Cesar Britto, 2020-02-28.
                                       *Se realiza el ajuste para cuando se crea perfil de pn
                                       */
                                         $("#id").val(result);
                                         notify("success", "ok", "Persona natural:", "Se actualizo con el éxito el participante como persona natural.");
-=======
-                                        notify("success", "ok", "Persona natural:", "Se actualizó con el éxito el participante como persona natural.");
->>>>>>> bec974ebb179930eb723d50ef95fdc9738234c04
                                     }
                                 }
                             }
